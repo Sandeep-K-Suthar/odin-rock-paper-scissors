3dc11b2e0bbabbc50d097148dedf224e67bc9e06
Sandeep-K-Suthar <sandeep.khemani@outlook.com>
2025-02-22 22:18:49 +0500

    add console instructions

--- rcs.js
+++ rcs.js
@@ -1,13 +1,15 @@
 
-// functions as encapsulation    
+// function as encapsulation    
 
 // computer play 
+
+console.log("Choose between {Rock, Paper, Scissors}\nPass the argument here as: humanDraw('Scissors')\n\nFor Example:"); //how to for console
 
 let computerPlay = ["Rock", "Paper", "Scissors"];
 function computerDraw() {
     let show = Math.floor(Math.random() * computerPlay.length);
 
-// human play
+    // human play
     function humanDraw(human) {
         console.log(`human choose: ${human}`);
         console.log(`computer choose: ${computerPlay[show]}`);
@@ -16,23 +18,29 @@ function computerDraw() {
             console.log('Draw!');
         }
         else {
-
-// win stratgy
+            // win stratgy
             switch (human) {
+
                 case "Rock":
-                    (computerPlay[show] == "Scissors") ? console.log(`human wins!`) : console.log(`computer wins!`);
+                    (computerPlay[show] == "Scissors") ?
+                        console.log(`human wins!`) :
+                        console.log(`computer wins!`);
                     break;
                 case "Paper":
-                    (computerPlay[show] == "Rock") ? console.log(`human wins!`) : console.log(`computer wins!`);
+                    (computerPlay[show] == "Rock") ?
+                        console.log(`human wins!`) :
+                        console.log(`computer wins!`);
                     break;
                 case "Scissors":
-                    (computerPlay[show] == "Paper") ? console.log(`human wins!`) : console.log(`computer wins!`);
+                    (computerPlay[show] == "Paper") ?
+                        console.log(`human wins!`) :
+                        console.log(`computer wins!`);
                     break;
             }
         }
     }
     humanDraw("Scissors");
 }
-computerDraw(computerPlay);       
+computerDraw(computerPlay);
 
 
