a4ede8371bf04955ac0bac2c1077c6bdc1f59aaa
Sandeep-K-Suthar <sandeep.khemani@outlook.com>
2025-02-22 20:16:37 +0500

    add functions as encapsulation

---
+++ rcs.js
@@ -0,0 +1,38 @@
+
+// functions as encapsulation    
+
+// computer play 
+
+let computerPlay = ["Rock", "Paper", "Scissors"];
+function computerDraw() {
+    let show = Math.floor(Math.random() * computerPlay.length);
+
+// human play
+    function humanDraw(human) {
+        console.log(`human choose: ${human}`);
+        console.log(`computer choose: ${computerPlay[show]}`);
+        if (human == computerPlay[show]) {
+
+            console.log('Draw!');
+        }
+        else {
+
+// win stratgy
+            switch (human) {
+                case "Rock":
+                    (computerPlay[show] == "Scissors") ? console.log(`human wins!`) : console.log(`computer wins!`);
+                    break;
+                case "Paper":
+                    (computerPlay[show] == "Rock") ? console.log(`human wins!`) : console.log(`computer wins!`);
+                    break;
+                case "Scissors":
+                    (computerPlay[show] == "Paper") ? console.log(`human wins!`) : console.log(`computer wins!`);
+                    break;
+            }
+        }
+    }
+    humanDraw("Scissors");
+}
+computerDraw(computerPlay);       
+
+
